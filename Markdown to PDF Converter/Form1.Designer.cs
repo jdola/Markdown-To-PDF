aa9bec00dce021beb8abccac0ee22ee4af30f6e1
Jdola <hoang2909@hotmail.com>
2025-03-10 19:50:38 +0700

    Markdown To Pdf v1.0

---
+++ Markdown to PDF Converter/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace Markdown_to_PDF_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabInput = new System.Windows.Forms.TabPage();
+            this.rtbMarkdownInput = new System.Windows.Forms.RichTextBox();
+            this.tabPreview = new System.Windows.Forms.TabPage();
+            this.wbPreview = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSavePdf = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1.SuspendLayout();
+            this.tabInput.SuspendLayout();
+            this.tabPreview.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabInput);
+            this.tabControl1.Controls.Add(this.tabPreview);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(760, 438);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabInput
+            // 
+            this.tabInput.Controls.Add(this.rtbMarkdownInput);
+            this.tabInput.Location = new System.Drawing.Point(4, 22);
+            this.tabInput.Name = "tabInput";
+            this.tabInput.Padding = new System.Windows.Forms.Padding(3);
+            this.tabInput.Size = new System.Drawing.Size(752, 412);
+            this.tabInput.TabIndex = 0;
+            this.tabInput.Text = "Markdown Input";
+            this.tabInput.UseVisualStyleBackColor = true;
+            // 
+            // rtbMarkdownInput
+            // 
+            this.rtbMarkdownInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbMarkdownInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbMarkdownInput.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbMarkdownInput.Location = new System.Drawing.Point(3, 3);
+            this.rtbMarkdownInput.Name = "rtbMarkdownInput";
+            this.rtbMarkdownInput.Size = new System.Drawing.Size(746, 406);
+            this.rtbMarkdownInput.TabIndex = 0;
+            this.rtbMarkdownInput.Text = "";
+            // 
+            // tabPreview
+            // 
+            this.tabPreview.Controls.Add(this.wbPreview);
+            this.tabPreview.Location = new System.Drawing.Point(4, 22);
+            this.tabPreview.Name = "tabPreview";
+            this.tabPreview.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPreview.Size = new System.Drawing.Size(752, 412);
+            this.tabPreview.TabIndex = 1;
+            this.tabPreview.Text = "HTML Preview";
+            this.tabPreview.UseVisualStyleBackColor = true;
+            // 
+            // wbPreview
+            // 
+            this.wbPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wbPreview.Location = new System.Drawing.Point(3, 3);
+            this.wbPreview.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wbPreview.Name = "wbPreview";
+            this.wbPreview.Size = new System.Drawing.Size(746, 406);
+            this.wbPreview.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnOpenFile);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.btnSavePdf);
+            this.panel1.Controls.Add(this.btnConvert);
+            this.panel1.Location = new System.Drawing.Point(12, 456);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(760, 44);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(660, 9);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 26);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSavePdf
+            // 
+            this.btnSavePdf.Location = new System.Drawing.Point(210, 9);
+            this.btnSavePdf.Name = "btnSavePdf";
+            this.btnSavePdf.Size = new System.Drawing.Size(123, 26);
+            this.btnSavePdf.TabIndex = 1;
+            this.btnSavePdf.Text = "Save as PDF";
+            this.btnSavePdf.UseVisualStyleBackColor = true;
+            this.btnSavePdf.Click += new System.EventHandler(this.btnSavePdf_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(92, 9);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(112, 26);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert to HTML";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(11, 9);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 26);
+            this.btnOpenFile.TabIndex = 3;
+            this.btnOpenFile.Text = "Open File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 509);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(39, 17);
+            this.statusLabel.Text = "Ready";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 531);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tabControl1);
+            //this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Markdown to PDF Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabInput.ResumeLayout(false);
+            this.tabPreview.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabInput;
+        private System.Windows.Forms.TabPage tabPreview;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSavePdf;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.RichTextBox rtbMarkdownInput;
+        private System.Windows.Forms.WebBrowser wbPreview;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.Button btnOpenFile;
+    }
+}
+
---
+++ Markdown to PDF Converter/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+using Markdig;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+using System.Text;
+
+namespace Markdown_to_PDF_Converter
+{
+    public partial class Form1 : Form
+    {
+        private string htmlContent = string.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string markdownText = rtbMarkdownInput.Text;
+
+                if (string.IsNullOrWhiteSpace(markdownText))
+                {
+                    MessageBox.Show("Please enter some Markdown text.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // Convert Markdown to HTML using Markdig
+                var pipeline = new MarkdownPipelineBuilder()
+                    .UseAdvancedExtensions()
+                    .Build();
+
+                htmlContent = Markdig.Markdown.ToHtml(markdownText, pipeline);
+
+                // Add some basic styling to the HTML
+                htmlContent = $@"<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {{ font-family: Arial, sans-serif; margin: 20px; }}
+        h1 {{ color: #333366; }}
+        h2 {{ color: #333366; }}
+        h3 {{ color: #333366; }}
+        pre {{ background-color: #f5f5f5; padding: 10px; border-radius: 5px; }}
+        code {{ font-family: Consolas, monospace; }}
+        blockquote {{ border-left: 4px solid #ccc; padding-left: 15px; color: #666; }}
+        table {{ border-collapse: collapse; width: 100%; }}
+        th, td {{ border: 1px solid #ddd; padding: 8px; }}
+        th {{ background-color: #f2f2f2; }}
+    </style>
+</head>
+<body>
+{htmlContent}
+</body>
+</html>";
+
+                wbPreview.DocumentText = htmlContent;
+                tabControl1.SelectedTab = tabPreview;
+
+                statusLabel.Text = "Markdown converted to HTML. Click 'Save as PDF' to export.";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error converting Markdown to HTML: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                statusLabel.Text = "Error occurred during conversion.";
+            }
+        }
+
+        private void btnSavePdf_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(htmlContent))
+            {
+                MessageBox.Show("Please convert Markdown to HTML first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "PDF files (*.pdf)|*.pdf",
+                Title = "Save PDF File",
+                DefaultExt = "pdf",
+                AddExtension = true
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    ConvertHtmlToPdf(htmlContent, saveFileDialog.FileName);
+                    statusLabel.Text = $"PDF saved successfully to: {saveFileDialog.FileName}";
+
+                    if (MessageBox.Show("PDF saved successfully. Do you want to open it?", "Success",
+                        MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start(saveFileDialog.FileName);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error saving PDF: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    statusLabel.Text = "Error occurred while saving PDF.";
+                }
+            }
+        }
+
+        private void ConvertHtmlToPdf(string html, string outputPath)
+        {
+            using (FileStream fs = new FileStream(outputPath, FileMode.Create))
+            {
+                // Create a PDF document
+                Document document = new Document(PageSize.A4, 50, 50, 60, 60);
+                PdfWriter writer = PdfWriter.GetInstance(document, fs);
+                document.Open();
+
+                // Convert HTML to PDF
+                using (StringReader sr = new StringReader(html))
+                {
+                    XMLWorkerHelper.GetInstance().ParseXHtml(writer, document, sr);
+                }
+
+                document.Close();
+                writer.Close();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            rtbMarkdownInput.Clear();
+            wbPreview.DocumentText = "";
+            htmlContent = string.Empty;
+            statusLabel.Text = "Ready";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rtbMarkdownInput.Text = "# Markdown to PDF Converter\n\nThis is a sample Markdown document to demonstrate the conversion functionality.\n\n## Features\n\n- Convert Markdown to HTML\n- Preview HTML rendering\n- Export to PDF\n\n## How to use\n\n1. Type or paste your Markdown text in the input area\n2. Click 'Convert to HTML' to preview\n3. Click 'Save as PDF' to export\n\n## Code Example\n\n```csharp\npublic class Example\n{\n    public static void Main()\n    {\n        Console.WriteLine(\"Hello, World!\");\n    }\n}\n```\n\n## Table Example\n\n| Name | Description |\n|------|-------------|\n| Markdown | Lightweight markup language |\n| PDF | Portable Document Format |\n\n> This is a blockquote example.";
+
+            statusLabel.Text = "Ready";
+        }
+
+        private void LoadMarkdownFile(string filePath)
+        {
+            try
+            {
+                string content = File.ReadAllText(filePath);
+                rtbMarkdownInput.Text = content;
+                statusLabel.Text = $"Loaded: {Path.GetFileName(filePath)}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Markdown files (*.md;*.markdown)|*.md;*.markdown|Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                Title = "Open Markdown File"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                LoadMarkdownFile(openFileDialog.FileName);
+            }
+        }
+    }
+}
